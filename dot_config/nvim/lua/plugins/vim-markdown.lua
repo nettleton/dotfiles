a5013b8736648df8b53d1f3b0c00d9444531e5a0
Joshua Wang <wang.joshua.k@gmail.com>
2021-10-24 16:04:14 -0400

    fix some markdown settings

--- dot_config/nvim/lua/plugins/vim-markdown.lua
+++ dot_config/nvim/lua/plugins/vim-markdown.lua
@@ -3,4 +3,16 @@
 -- vim.g.vim_markdown_frontmatter = 1  -- for YAML format
 
 -- MATLAB syntax highlighting
-vim.g.vim_markdown_fenced_languages = "['matlab=octave']"
+-- vim.g.vim_markdown_fenced_languages = "['matlab=octave']"
+
+-- disable header folding
+vim.g.vim_markdown_folding_disabled = 1
+
+-- do not require .md extensions for Markdown links
+vim.g.vim_markdown_no_extensions_in_markdown = 1
+
+-- auto-write when following link
+vim.g.vim_markdown_autowrite = 1
+
+-- open links in new tab
+vim.g.vim_markdown_edit_url_in = 'tab'
