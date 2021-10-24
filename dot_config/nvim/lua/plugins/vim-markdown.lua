ce379cb28b380b917c57a23c568dcfc0e87c6228
Joshua Wang <wang.joshua.k@gmail.com>
2021-10-24 02:07:21 -0400

    MATLAB fenced language

--- dot_config/nvim/lua/plugins/vim-markdown.lua
+++ dot_config/nvim/lua/plugins/vim-markdown.lua
@@ -1,3 +1,6 @@
 -- support front matter
 -- enabling frontmatter makes links unreachable
 -- vim.g.vim_markdown_frontmatter = 1  -- for YAML format
+
+-- MATLAB syntax highlighting
+vim.g.vim_markdown_fenced_languages = ['matlab=octave']
