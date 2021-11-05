b2db95a38d00e039fe478d31a16a4f020514c0aa
Joshua Wang <github@nettleton.io>
2021-11-05 11:48:32 -0400

    generate diary index

---
+++ dot_config/fish/functions/generateDiaryIndex.fish
@@ -0,0 +1,33 @@
+function generateDiaryIndex -d "generate diary index"
+
+  set INDEX "$NOTES/diary/diary.md"
+
+  set MONTHS "January" "February" "March" "April" "May" "June" "July" "August" "September" "October" "November" "December"
+
+  echo "# Diary" > "$INDEX"
+  echo "" >> "$INDEX"
+
+  set writtenHeaders ""
+
+  for fn in (find $NOTES/diary -name "[0-9]*\.md" | sort -r);
+    set bn (basename "$fn")
+    set YMD (string split -f1 _ "$bn" | string split -)
+
+    set yearHeader "## $YMD[1]"
+    set monthHeader "### $MONTHS[$YMD[2]] $YMD[1]"
+
+    if not contains "$yearHeader" $writtenHeaders
+      echo "$yearHeader" >> "$INDEX"
+      set writtenHeaders $writtenHeaders "$yearHeader"
+    end
+
+    if not contains "$monthHeader" $writtenHeaders
+      echo "" >> "$INDEX"
+      echo "$monthHeader" >> "$INDEX"
+      echo "" >> "$INDEX"
+      set writtenHeaders $writtenHeaders "$monthHeader"
+    end
+
+    echo "- [$bn](./$bn)" >> "$INDEX"
+  end
+end
