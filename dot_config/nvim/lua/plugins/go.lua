0a3a6bb5ca77d5f55478db3beb355c3a7f296782
Joshua Wang <github@nettleton.io>
2022-01-21 21:44:39 -0500

    abstract some things

--- dot_config/nvim/lua/plugins/go.lua
+++ dot_config/nvim/lua/plugins/go.lua
@@ -6,15 +6,14 @@ require('go').setup()
 
 require("nvim-dap-virtual-text").setup()
 
+require('lspconfig').gopls.setup{}
+
 -- 1. format on save
 -- 2. import on save
--- TODO: this erases valid code
--- exec([[
--- autocmd BufWritePre *.go lua require('go.format').gofmt()
--- autocmd BufWritePre *.go lua require('go.format').goimport()
--- ]], false)
-
+-- Run gofmt + goimport on save
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
 
 -- REFERENCES
 -- https://github.com/ray-x/go.nvim
 --   the linked treesitter config is actually https://github.com/ray-x/dotfiles/blob/zprezto-plug/lua/treesitter.lua
+-- https://github.com/ray-x/navigator.lua/blob/master/lua/navigator/lspclient/clients.lua
