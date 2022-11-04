0105cc0fea0378932e42cfaff5ed3c986fd846eb
Joshua Wang <github@nettleton.io>
2022-11-04 19:53:35 -0400

    cmp_nvim_lsp.update_capabilities is deprecated - https://github.com/hrsh7th/cmp-nvim-lsp/issues/38

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -96,15 +96,11 @@ mason_tool_installer.setup {
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#jsonls
 
 -- Enable (broadcasting) snippet capability for completion
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local lsp_defaults = {
   flags = {
     debounce_text_changes = 150,
   },
-  capabilities = cmp_nvim_lsp.update_capabilities(
-    capabilities
-  ),
+  capabilities = cmp_nvim_lsp.default_capabilities(),
 }
 
 lspconfig.util.default_config = vim.tbl_deep_extend(
