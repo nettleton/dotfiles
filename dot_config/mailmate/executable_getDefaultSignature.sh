fecc76d1eb2148fe65f53b63b412dedf453ea9aa
Joshua Wang <github@nettleton.io>
2022-12-02 17:23:38 -0500

    get mailmate default signature

---
+++ dot_config/mailmate/executable_getDefaultSignature.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUM_SIGNATURES=$(/usr/libexec/PlistBuddy -c "Print :" ~/Library/Application\ Support/MailMate/Signatures.plist | grep -c uuid)
+
+for idx in 0 $(($NUM_SIGNATURES - 1)); do
+  SIG_NAME=$(/usr/libexec/PlistBuddy -c "Print :$idx:name" "$HOME/Library/Application Support/MailMate/Signatures.plist")
+  if [ "$SIG_NAME" == "Default" ]; then 
+    UUID=$(/usr/libexec/PlistBuddy -c "Print :$idx:uuid" "$HOME/Library/Application Support/MailMate/Signatures.plist")
+  fi
+done
+
+echo "$UUID"
