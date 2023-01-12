e98b085b17825ae8123767b86a67498c70f025a7
Joshua Wang <github@nettleton.io>
2023-01-12 02:15:24 -0500

    make formatting consistent

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -52,8 +52,12 @@ return packer.startup(function(use)
         requires = {
           { 'godlygeek/tabular' }
         },
-        config = [[require('plugins.vim-markdown')]] }
+        config = function()
+          require('plugins.vim-markdown')
+        end,
+      }
   use { 'gaoDean/autolist.nvim',
+        after = { 'vim-markdown', 'nvim-autopairs' },
         config = function()
           local autolist = require("autolist")
           autolist.setup()
@@ -76,41 +80,40 @@ return packer.startup(function(use)
       }
 
   -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-    config = [[require('plugins.telescope')]],
-  }
+  use { 'nvim-telescope/telescope.nvim',
+        config = function()
+          require('plugins.telescope')
+        end,
+      }
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use { 'cljoly/telescope-repo.nvim',
-    requires = {
-      { 'nvim-lua/plenary.nvim' }
-    }
-  }
-  use {
-    'AckslD/nvim-neoclip.lua',
-    requires = {
-      {'kkharji/sqlite.lua', module = 'sqlite'},
-      {'nvim-telescope/telescope.nvim'},
-    },
-    config = function()
-      require('neoclip').setup()
-    end,
-  }
-  use {
-    'sudormrfbin/cheatsheet.nvim',
+        requires = {
+          { 'nvim-lua/plenary.nvim' }
+        }
+      }
+  use { 'AckslD/nvim-neoclip.lua',
+        requires = {
+          {'kkharji/sqlite.lua', module = 'sqlite'},
+          {'nvim-telescope/telescope.nvim'},
+        },
+        config = function()
+          require('neoclip').setup()
+        end,
+      }
+  use { 'sudormrfbin/cheatsheet.nvim',
+        requires = {
+          {'nvim-telescope/telescope.nvim'},
+          {'nvim-lua/popup.nvim'},
+          {'nvim-lua/plenary.nvim'},
+        },
 
-    requires = {
-      {'nvim-telescope/telescope.nvim'},
-      {'nvim-lua/popup.nvim'},
-      {'nvim-lua/plenary.nvim'},
-    },
-
-    config = [[require('cheatsheet').setup()]]
-  }
-  use {
-    "benfowler/telescope-luasnip.nvim",
-    module = "telescope._extensions.luasnip",  -- if you wish to lazy-load
-  }
+        config = function()
+          require('cheatsheet').setup()
+        end,
+      }
+  use { "benfowler/telescope-luasnip.nvim",
+        module = "telescope._extensions.luasnip",  -- if you wish to lazy-load
+      }
   use { 'nvim-telescope/telescope-file-browser.nvim' }
   use { 'nvim-telescope/telescope-packer.nvim' }
   use {
@@ -125,65 +128,85 @@ return packer.startup(function(use)
 
   -- Git
   use { 'tpope/vim-fugitive', event = 'VimEnter' } -- Git wrapper for vim
-  use {
-    'tpope/vim-rhubarb',
-    requires = { 'tpope/vim-fugitive' },
-    event = 'VimEnter'
-  } -- Open files in GitHub UI
-  use { 'lewis6991/gitsigns.nvim', config = [[require('plugins.gitsigns')]] } -- Git signs
+  use { 'tpope/vim-rhubarb',
+        requires = { 'tpope/vim-fugitive' },
+        event = 'VimEnter'
+      } -- Open files in GitHub UI
+  use { 'lewis6991/gitsigns.nvim',
+        config = function()
+          require('plugins.gitsigns')
+        end,
+      } -- Git signs
   use { 'rhysd/git-messenger.vim', event = 'VimEnter' } -- Show Git info in a popup
 
   -- Golang
   use { 'ray-x/go.nvim',
-    config = function()
-      require('go').setup({
-        gofmt = 'gofmt',
-        max_line_len = 999,
-      })
-      -- Run gofmt + goimport on save
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*.go",
-        callback = function()
-         require('go.format').goimport()
+        config = function()
+          require('go').setup({
+            gofmt = 'gofmt',
+            max_line_len = 999,
+          })
+          -- Run gofmt + goimport on save
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+             require('go.format').goimport()
+            end,
+            group = format_sync_grp,
+          })
         end,
-        group = format_sync_grp,
-      })
-
-    end,
-  }
+      }
   use {'ray-x/guihua.lua', run = 'cd lua/fzy && make'}
-  use { 'leoluz/nvim-dap-go', config = [[require('dap-go').setup()]] }
+  use { 'leoluz/nvim-dap-go',
+        config = function()
+          require('dap-go').setup()
+        end,
+      }
 
   -- DAP / debugging
   use { 'mfussenegger/nvim-dap' }
-  use {
-    'rcarriga/nvim-dap-ui',
-    requires = { 'mfussenegger/nvim-dap' },
-    config = [[require('dapui').setup()]]
-  }
-  use { 'theHamsta/nvim-dap-virtual-text', config = [[require('nvim-dap-virtual-text').setup()]] }
+  use { 'rcarriga/nvim-dap-ui',
+        requires = { 'mfussenegger/nvim-dap' },
+        config = function()
+          require('dapui').setup()
+        end,
+      }
+  use { 'theHamsta/nvim-dap-virtual-text',
+        config = function()
+          require('nvim-dap-virtual-text').setup()
+        end,
+      }
   use { 'nvim-telescope/telescope-dap.nvim' }
 
   -- Appearance and themes
   use { 'sainnhe/sonokai',
-    config = function()
-      vim.g.sonokai_style = 'shusia'
-      vim.g.sonokai_enable_italic = 1
-      vim.cmd[[colorscheme sonokai]]
-    end,
-  }
+        config = function()
+          vim.g.sonokai_style = 'shusia'
+          vim.g.sonokai_enable_italic = 1
+          vim.cmd[[colorscheme sonokai]]
+        end,
+      }
 
   -- Statusline
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-    config = [[require('plugins.lualine')]]
-  }
-  use { 'akinsho/nvim-bufferline.lua', config = [[require('plugins.nvim-bufferline')]] } -- Better nvim buffers
+  use { 'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+        config = function()
+          require('plugins.lualine')
+        end,
+      }
+  use { 'akinsho/nvim-bufferline.lua',
+        config = function()
+          require('plugins.nvim-bufferline')
+        end,
+      } -- Better nvim buffers
 
   -- Autocompletion, formatting, linting & intellisense
   -- cmp plugins
-  use { 'hrsh7th/nvim-cmp', config = [[require('plugins.cmp')]] } -- The completion plugin
+  use { 'hrsh7th/nvim-cmp',
+        config = function()
+          require('plugins.cmp')
+        end,
+      } -- The completion plugin
   use { 'hrsh7th/cmp-buffer' } -- buffer completions
   use { 'hrsh7th/cmp-path' } -- path completions
   use { 'hrsh7th/cmp-cmdline' } -- cmdline completions
@@ -196,7 +219,11 @@ return packer.startup(function(use)
   use { 'rcarriga/cmp-dap' } -- nvim-dap
 
   -- cmp snippets
-  use { 'L3MON4D3/LuaSnip', config = [[require('luasnip.loaders.from_vscode').lazy_load({paths="~/.config/nvim/snips"})]] } --snippet engine
+  use { 'L3MON4D3/LuaSnip',
+        config = function()
+          require('luasnip.loaders.from_vscode').lazy_load({paths="~/.config/nvim/snips"})
+        end,
+      } --snippet engine
   use { 'rafamadriz/friendly-snippets' } -- a bunch of snippets to use
 
   -- LSP
@@ -206,43 +233,74 @@ return packer.startup(function(use)
   use { 'neovim/nvim-lspconfig' }
   use { 'b0o/schemastore.nvim' }
   use { 'jose-elias-alvarez/null-ls.nvim' }
-  use {
-    "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- https://github.com/folke/trouble.nvim/
+  use { "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+        config = function()
+          require("trouble").setup {
+            -- your configuration comes here
+            -- https://github.com/folke/trouble.nvim/
+          }
+        end
       }
-    end
-  }
 
   -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-    config = [[require('plugins.treesitter')]]
-  }
+  use { 'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+        config = function()
+          require('plugins.treesitter')
+        end,
+      }
   use { 'nvim-treesitter/nvim-treesitter-textobjects' }
   use { 'p00f/nvim-ts-rainbow' }
-  use { 'simrat39/symbols-outline.nvim', config = [[require('symbols-outline').setup()]] }
+  use { 'simrat39/symbols-outline.nvim',
+        config = function()
+          require('symbols-outline').setup()
+        end,
+      }
 
   -- Utilities
-  use { 'windwp/nvim-autopairs', config = [[require('plugins.nvim-autopairs')]] } -- Insert or delete brackets, parens, quotes in pair.
-  use { 'numtostr/comment.nvim', config = [[require('plugins.comment')]] } -- Comment stuff out easily
-  use { 'miyakogi/conoline.vim', config = [[require('plugins.conoline')]] } -- Highlight the line of the cusor in the current window
-  use { 'airblade/vim-rooter', config = [[require('plugins.vim-rooter')]] } -- Change vim working directory to project directory
+  use { 'windwp/nvim-autopairs',
+        config = function()
+          require('plugins.nvim-autopairs')
+        end
+      } -- Insert or delete brackets, parens, quotes in pair.
+  use { 'numtostr/comment.nvim',
+        config = function()
+          require('plugins.comment')
+        end,
+      } -- Comment stuff out easily
+  use { 'miyakogi/conoline.vim',
+        config = function()
+          require('plugins.conoline')
+        end,
+      } -- Highlight the line of the cursor in the current window
+  use { 'airblade/vim-rooter',
+        config = function()
+          require('plugins.vim-rooter')
+        end,
+      } -- Change vim working directory to project directory
   use { 'fladson/vim-kitty' } -- highlighting support for kitty config
   use { 'alker0/chezmoi.vim' } -- highlighting support for chezmoi templates
 
-  use { 'folke/which-key.nvim', config = [[require('plugins.which-key')]] } -- key bindings
+  use { 'folke/which-key.nvim',
+        config = function()
+          require('plugins.which-key')
+        end,
+      } -- key bindings
 
   -- strip trailing whitespace from edited lines
-  use {
-    'lewis6991/spaceless.nvim', config = [[require('spaceless').setup()]]}
+  use { 'lewis6991/spaceless.nvim',
+        config = function()
+          require('spaceless').setup()
+        end,
+      }
 
   -- ZK
-  use { 'mickael-menu/zk-nvim', config = [[require('zk').setup()]] }
+  use { 'mickael-menu/zk-nvim',
+        config = function()
+          require('zk').setup()
+        end,
+      }
 
   if PACKER_BOOTSTRAP then
     require('packer').sync()
