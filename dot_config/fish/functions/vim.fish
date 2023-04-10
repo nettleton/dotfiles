7452fe119be0f5a4e3e3fddef6e2c86f02a01ec3
Joshua Wang <github@nettleton.io>
2023-04-10 09:39:35 -0400

    StopTheFonts

--- dot_config/fish/functions/vim.fish
+++ dot_config/fish/functions/vim.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function vim
-  set activeTabTitle (kitty @ --to unix:/tmp/kitty ls | jq -r '.[] | select(.is_active == true) | .tabs[] | select(.is_active_tab == true) | .title')
+  set activeTabTitle (kitty @ --to unix:/tmp/kitty ls | jq -r '.[] | select(.is_active == true) | .tabs[] | select(.is_active == true) | .title')
   switch $activeTabTitle
     case scratch
       nvim --server /tmp/scratch_editor --remote-silent $PWD/$argv
