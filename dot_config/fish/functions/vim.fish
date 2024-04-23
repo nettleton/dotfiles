4e5e30562b787282b414ae9ba1ad8c1eee5447e2
Joshua Wang <github@nettleton.io>
2024-04-23 11:37:12 -0400

    handle relative and absolute paths

--- dot_config/fish/functions/vim.fish
+++ dot_config/fish/functions/vim.fish
@@ -2,14 +2,20 @@
 
 function vim
   set activeTabTitle (kitty @ --to unix:/tmp/kitty ls | jq -r '.[] | select(.is_active == true) | .tabs[] | select(.is_active == true) | .title')
+
+  set fullPathToFile "$PWD/$argv"
+  if string match -rq '^/.*' (dirname "$argv")
+    set fullPathToFile "$argv"
+  end
+
   switch $activeTabTitle
     case scratch
-      nvim --server /tmp/scratch_editor --remote-silent $PWD/$argv
+      nvim --server /tmp/scratch_editor --remote-silent "$fullPathToFile"
       kitty @ --to unix:/tmp/kitty focus-window --match 'title:scratch'
     case notes
-      nvim --server /tmp/notes_spacevim_nvim_server --remote-silent $PWD/$argv
+      nvim --server /tmp/notes_spacevim_nvim_server --remote-silent "$fullPathToFile"
       kitty @ --to unix:/tmp/kitty focus-window --match 'title:notes'
     case '*'
-      echo "$argv[1]" | grep -iq 'Documents/Notes' ; and nvim --listen /tmp/notes_spacevim_nvim_server $argv ; or nvim $argv
+      echo "$argv[1]" | grep -iq 'Documents/Notes' ; and nvim --listen /tmp/notes_spacevim_nvim_server "$fullPathToFile" ; or nvim "$fullPathToFile"
   end
 end
