4629334f377d1c15198c995a32480cc78459703e
Joshua Wang <github@nettleton.io>
2024-04-28 01:19:20 -0400

    add zk to vim-rooter pattern

--- dot_config/nvim/lua/plugins/vim-rooter.lua
+++ dot_config/nvim/lua/plugins/vim-rooter.lua
@@ -1,2 +1,2 @@
-vim.g.rooter_patterns = { '!^target','go.mod','pom.xml','!^package.json','.git','index.md' }
+vim.g.rooter_patterns = { '!^target','go.mod','pom.xml','!^package.json','.git','index.md','.zk' }
 vim.g['rooter_cd_cmd'] = 'lcd'
